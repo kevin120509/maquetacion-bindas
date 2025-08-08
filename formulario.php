f2274c8495d0a2ced1d397af4d03e62fd8db2acc
kevin120509 <124070470+kevin120509@users.noreply.github.com>
2025-08-08 10:53:09 -0600

    Improve contact form design and enable mail

--- formulario.php
+++ formulario.php
@@ -3,19 +3,24 @@
 $destino = "kevin.36137@gmail.com";
 $asunto = "Contacto desde el sitio web";
 
+$nombre = htmlspecialchars($_POST["nombre"] ?? '');
+$apellido = htmlspecialchars($_POST["apellido"] ?? '');
+$email = htmlspecialchars($_POST["email"] ?? '');
+$comentarios = htmlspecialchars($_POST["comentarios"] ?? '');
 
-$nombre = $_POST["nombre"];
-$apellido = $_POST["apellido"];
-$email = $_POST["email"];
-$comentarios = $_POST["comentarios"];
-$contenido = "
-	<b>Nombre :</b> " . $nombre . "<br><b>Apellido :</b>" . $apellido . "<br><b>Email :</b> " . $email . "<br><b>Comentarios : </b> " . $comentarios;
+$contenido = "<b>Nombre :</b> $nombre<br><b>Apellido :</b> $apellido<br><b>Email :</b> $email<br><b>Comentarios : </b> $comentarios";
 
 // PARA QUE RECONOZCA LAS ETIQUETAS HTML
 
 $encabezados  = "MIME-Version: 1.0" . "\r\n";
 $encabezados .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$encabezados .= "From: $nombre $apellido <$email>" . "\r\n";
 
 // FUNCION MAIL Y REDIRECCIONAMIENTO
-mail($destino, $asunto, $contenido, $encabezados);
-header("Location: gracias.html");
+if (mail($destino, $asunto, $contenido, $encabezados)) {
+    header("Location: gracias.html");
+    exit;
+} else {
+    echo "Error al enviar el mensaje";
+}
+
